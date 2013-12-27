e8b4f8a0bc3a5106274243abbe9a8b291aee785e
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-26 23:02:19 -0500

    work on prototype client

---
+++ socket_io_test.rb
@@ -0,0 +1,64 @@
+
+require 'net/https'
+require 'uri'
+require 'websocket/driver'
+
+module MessageParser
+  REGEX = /([^:]+):([0-9]+)?(\+)?:([^:]+)?:?([\s\S]*)?/
+
+  # returns hash as {type: '1', id: '1', end_point: '4', data: [{key: value}]}
+  def decode(string)
+    (pieces = string.match(@regexp)) ? format(pieces) : {type: '0'}
+  end
+
+  def format(pieces)
+    {type: pieces[1], id: pieces[2], end_point: pieces[4], data: pieces[5]}
+  end
+
+  module_function :decode, :format
+end
+
+module UnvalidatedGet
+  def perform(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    # The cert is expired on our primary connection, until that is changed we
+    # need to disable verification to make a connection at all.
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    response = http.request(Net::HTTP::Get.new(uri.request_uri))
+    response.body
+  end
+
+  module_function :perform
+end
+
+module Session
+  def format_url(url)
+    uri = URI.parse(url)
+    uri.path = '/socket.io/1/'
+    uri.port = uri.port || 443
+    uri.query = "t=#{Time.now.to_i}"
+    uri
+  end
+
+  def get_session(url)
+    UnvalidatedGet.perform(format_url(url)).split(':')
+  end
+
+  module_function :format_url, :get_session
+end
+
+class SocketIOClient
+  def initialize(url)
+    session_values = Session.get_session(url)
+
+    @session_id         = session_values[0]
+    @heartbeat_timeout  = session_values[1]
+    @connection_timeout = session_values[2]
+    @transports         = session_values[3].split(',')
+  end
+end
+
+sioc = SocketIOClient.new('https://socketio.mtgox.com/')
