f7bcc8e322525e9d42a1d85fe22805ae0e4be609
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-12-28 22:43:57 -0500

    Getting closer to having minutely candlestick data

--- lib/trade_bot/actors/data_processing.rb
+++ lib/trade_bot/actors/data_processing.rb
@@ -1,4 +1,5 @@
 
+require 'date'
 require 'net/https'
 require 'pubnub'
 
@@ -62,6 +63,29 @@ module TradeBot
           redis.hset('trading:current', key, val)
         end
         redis.zadd('trading:data', ticker['now'], JSON.generate(instaneous_value))
+      end
+
+      # Check to see if we need to process this minute's worth of data
+      cur_time = Time.now.to_i
+      minute_start = Time.at(cur_time - (cur_time % 60))
+
+      # Either no data has been processed, or at least one minute has passed
+      # and data needs to be processed.
+      last_minute_processed = redis.get('trading:processed:minutely')
+      if last_minute_processed.nil? || minute_start >= Time.parse(last_minute_processed)
+        # Figure out the time score of the first entry needing processing
+        start_value = (last_minute_processed ? (Time.parse(last_minute_processed).to_i * 1e6).to_i : 0)
+        _, time = redis.zrange("trading:data", start_value, 1, with_scores: true).first
+
+        info(time.to_i)
+        info((minute_start.to_i * 1e6).to_i)
+        #while time <= (minute_start.to_i * 1e6)
+          relevant = redis.zrangebyscore('trading:data', time, time + (60 * 1e6))
+          info(relevant.size)
+
+          # Increment to the next 60 seconds
+          time += 60 * 1e6
+        #end
       end
 
       info(instaneous_value)
